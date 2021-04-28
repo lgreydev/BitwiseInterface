8293902a126dd745725c255db5a7b3c7573f4da4
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-04-28 23:02:08 +0300

    Add UI interfece

--- ControlledPractice/ViewController.swift
+++ ControlledPractice/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Property
+    @IBOutlet weak var counterButton: UIButton!
+    @IBOutlet var switches: [UISwitch]!
+    @IBOutlet weak var mySlider: UISlider!
+    @IBOutlet weak var myTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // MARK: - Methods
+    @IBAction func pressCounterButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func pressSwitch(_ sender: UISwitch) {
+    }
+    
+    @IBAction func moveSlider(_ sender: UISlider) {
+    }
+    
+    @IBAction func editorTextField(_ sender: UITextField) {
+    }
+    
+    
 }
 
