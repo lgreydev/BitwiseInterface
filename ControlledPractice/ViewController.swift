ee3c0a93263b90e3c1ea27ae65a6cd6d2e2ff1ea
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-04-30 01:12:08 +0300

    Update slider, textfield, button

--- ControlledPractice/ViewController.swift
+++ ControlledPractice/ViewController.swift
@@ -15,23 +15,60 @@ class ViewController: UIViewController {
     @IBOutlet weak var mySlider: UISlider!
     @IBOutlet weak var myTextField: UITextField!
     
+    var number: UInt8 = 128 {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    // MARK: - View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
     
-    // MARK: - Methods
-    @IBAction func pressCounterButton(_ sender: UIButton) {
+    
+    
+    // MARK: - Update UI
+    /// Update all outlet
+    func updateUI() {
+        counterButton.setTitle("\(number)", for: .normal)
         
+        // TODO: set switchers to number
+        
+        mySlider.value = Float(number)
+        myTextField.text = String(number)
     }
     
+    
+    
+    // MARK: - Button Action
+    @IBAction func pressCounterButton(_ sender: UIButton) {
+        if number == 255 {
+            number = 0
+        } else  {
+            number += 1
+        }
+    }
+    
+    
+    
+    // MARK: - Switch Action
     @IBAction func pressSwitch(_ sender: UISwitch) {
     }
     
+    
+    // MARK: - Slider Action
     @IBAction func moveSlider(_ sender: UISlider) {
+        number = UInt8(sender.value)
     }
     
+    
+    
+    // MARK: - Text Field Action
+    
     @IBAction func editorTextField(_ sender: UITextField) {
+        
     }
     
     
